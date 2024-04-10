547ff45ce9e71579977d3f82d95519bb15f1b425
Ed Eykholt <ed.eykholt@gmail.com>
2024-04-10 10:19:45 -0700

    Working sample of JSImport in keriauth.BrowserExtension.BlazorApp

---
+++ keriauth.BrowserExtension.BlazorApp/Services/SignifyTsInterop.cs
@@ -0,0 +1,25 @@
+﻿// using Microsoft.JSInterop;
+// using MudBlazor;
+using System;
+using System.Runtime.Versioning;
+using System.Text.Json;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace keriauth.BrowserExtension.BlazorApp;
+
+[SupportedOSPlatform("browser")]
+public partial class SignifyTsInterop
+{
+    //private readonly IJSRuntime jSRuntime;
+    //public SignifyTsInterop(IJSRuntime jSRuntime)
+    //{
+    //    this.jSRuntime = jSRuntime;
+    //}
+
+    // keep the imported method and property names aligned with SignifyTsInterop.ts
+    
+    [JSImport("getMessage", "signifyTsInterop")] // "/scripts/SignifyTsInterop.js")]
+    internal static partial string GetMessage();
+}
