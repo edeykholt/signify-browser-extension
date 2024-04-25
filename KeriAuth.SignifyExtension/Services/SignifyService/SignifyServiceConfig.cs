334c77e4d8570ddb3e11923dac36a6a16872e6a8
Ed Eykholt <ed.eykholt@gmail.com>
2024-04-25 12:55:26 -0700

    comments

--- KeriAuth.SignifyExtension/Services/SignifyService/SignifyServiceConfig.cs
+++ KeriAuth.SignifyExtension/Services/SignifyService/SignifyServiceConfig.cs
@@ -4,7 +4,7 @@
     {
         // See Signify/KERIA Request Authentication Protocol (SKRAP), https://github.com/WebOfTrust/keria/blob/development/docs/protocol.md
         public const string MessageRouterPort = "3902"; // aka KERI Protocol Interface, including oobi
-        public const string BootPort = "3903";   // for boot, health, and spec.yaml
-        public const string AdminPort = "3901";  // for API Handler, Agent API Requests
+        public const string BootPort = "3903";   // for boot and health
+        public const string AdminPort = "3901";  // for API Handler, Agent API Requests, and spec.yaml
     }
 }
